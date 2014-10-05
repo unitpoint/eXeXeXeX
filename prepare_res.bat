9d63557e369895de5cf702e30ac0c70d3986ab0b
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2014-10-05 13:06:17 +0400

    add company logo screen

--- prepare_res.bat
+++ prepare_res.bat
@@ -4,3 +4,4 @@ cd ..
 
 python ..\..\oxygine-framework\tools\process_xml2.py -x xmls/sounds.xml --src_data data --dest_data data
 python ..\..\oxygine-framework\tools\process_xml2.py -x xmls/game.xml --src_data data --dest_data data
+python ..\..\oxygine-framework\tools\process_xml2.py -x xmls/logo.xml --src_data data --dest_data data
